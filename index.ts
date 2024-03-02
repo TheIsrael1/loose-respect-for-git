fc5690d05259f8fd67dec1403bb855b75378ebfb
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-03-02 14:16:03 +0100

    Learn about staging changes seprately to commit

--- index.ts
+++ index.ts
@@ -7,5 +7,22 @@ Every git repository has one or more branches. A branch is a named reference to 
 sequence of commits.
 */
 
+/* 
+git commit --amend --no-edit
+add new change to the last commit
+*/
+
+/* 
+git commit -m will skip the text editor and directly commit the changes also
+
+git commit -a  will add all the changes to the staging area and then commit the changes
+
+dont need to add -a with -m
+*/
+
+/* 
+git commit -m "Commit summary" -m "More detailed description follows here"
+*/
+
 
 /*  new change  1*/  

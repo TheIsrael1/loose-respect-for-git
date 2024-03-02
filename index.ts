7205659ef94752179ad847f2e28096f9c7ff83ad
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-03-02 12:12:46 +0100

    i made some changes:

---
+++ index.ts
@@ -0,0 +1,11 @@
+const res = 2 + 2
+
+console.log(res)
+
+/* 
+Every git repository has one or more branches. A branch is a named reference to the HEAD of a
+sequence of commits.
+*/
+
+
+/*  new change */  
